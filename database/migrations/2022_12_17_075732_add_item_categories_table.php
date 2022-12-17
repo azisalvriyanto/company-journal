d36e79ebab1518a786fd102764f562ac45128c63
azisalvriyanto <aalvriyanto@gmail.com>
2022-12-17 15:06:59 +0700

    [item categories] make migration

---
+++ database/migrations/2022_12_17_075732_add_item_categories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('item_categories', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('owner_id')->index();
+            $table->foreign('owner_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
+            $table->string('name');
+            $table->string('image_url')->nullable();
+            $table->boolean('is_enable')->default(1);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('item_categories');
+    }
+};
