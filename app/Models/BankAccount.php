71cab845d7994b3b685772f04bedc6dfcca6a2b4
azisalvriyanto <aalvriyanto@gmail.com>
2023-01-28 05:36:55 +0700

    [bank-account] add migration and set model

---
+++ app/Models/BankAccount.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Concerns\UsesUuid;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class BankAccount extends Model
+{
+    use SoftDeletes, UsesUuid;
+
+    protected $guarded = [];
+
+    public function owner()
+    {
+        return $this->belongsTo(User::class, 'owner_id', 'id');
+    }
+}
