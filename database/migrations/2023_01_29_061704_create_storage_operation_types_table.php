2ab1ab045cde9c7e8a6a91de77a9275e64189680
azisalvriyanto <aalvriyanto@gmail.com>
2023-01-29 13:36:05 +0700

    [storage-operating-types] add migration and model

---
+++ database/migrations/2023_01_29_061704_create_storage_operation_types_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('storage_operation_types', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->enum('group', ['In', 'Out']);
+            $table->string('name');
+            $table->string('transaction_code');
+            $table->boolean('is_enable')->default(1);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('storage_operation_types');
+    }
+};
