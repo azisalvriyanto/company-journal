ceb2697c6f1e9213b5009ed290668609745f532f
azisalvriyanto <aalvriyanto@gmail.com>
2023-03-04 13:10:18 +0700

    [sales-order] add migration and model

---
+++ database/migrations/2023_03_04_055041_create_sales_orders_table.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales_orders', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('monthly_journal_id')->index();
+            $table->foreign('monthly_journal_id')->references('id')->on('monthly_journals')->onUpdate('cascade')->onDelete('cascade');
+
+            $table->datetime('transaction_time')->nullable();
+            $table->datetime('order_deadline')->nullable();
+
+            $table->foreignUuid('payment_term_id')->index();
+            $table->foreign('payment_term_id')->references('id')->on('payment_terms')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignUuid('vendor_id')->index();
+            $table->foreign('vendor_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignUuid('vendor_address_id')->index();
+            $table->foreign('vendor_address_id')->references('id')->on('contacts')->onUpdate('cascade')->onDelete('cascade');
+
+            $table->string('code')->nullable();
+            $table->string('internal_code')->nullable();
+
+            $table->foreignUuid('invoice_id')->nullable()->index();
+            $table->foreign('invoice_id')->references('id')->on('invoices')->onUpdate('cascade')->onDelete('cascade');
+
+            $table->text('note')->nullable();
+
+            $table->double('total_sales', 50, 10)->default(0);
+
+            $table->foreignUuid('status_id')->index();
+            $table->foreign('status_id')->references('id')->on('statuses')->onUpdate('cascade')->onDelete('cascade');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales_orders');
+    }
+};
